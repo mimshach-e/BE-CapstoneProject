6bfd47d062f175e56a641f0d629acb9083ce50d0
Ernest Mimshach <mimshach.e@gmail.com>
2024-10-14 18:50:33 +0000

    Updated ALLOWED_HOSTS for Heroku domain

--- e_commerce_api/settings.py
+++ e_commerce_api/settings.py
@@ -33,8 +33,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-ALLOWED_HOSTS.append(os.environ.get('ALLOWED_HOST', ''))
+ALLOWED_HOSTS = [
+    'heroku-e-commerce-app-85cac8f4a57c.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Security settings
 SECURE_BROWSER_XSS_FILTER = True
